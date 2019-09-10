580accf0827b6a0d43b3410ef405413c6c1bc777
while95 <l-pot@naver.com>
2019-09-10 15:35:02 +0900

    Create main.cpp

---
+++ 백준3613자바시플플/백준3613자바시플플/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+   
+    string a;
+    cin>>a;
+    
+    if(a.find("_")>0){
+        
+    }
+    
+    
+    return 0;
+}
